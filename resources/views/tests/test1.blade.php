ec5c14927db545af6da0cd259fa705eec4144bb0
harada <pokosuke1004@gmail.com>
2025-02-25 20:05:46 +0900

    属性バック

--- resources/views/tests/test1.blade.php
+++ resources/views/tests/test1.blade.php
@@ -3,4 +3,8 @@
   コンポーネントテスト１
 
   <x-tests.card title="タイトル1" content="内容です1" :message="$message"/>
+  <x-tests.card title="タイトル2" content="あ"/>
+  <x-tests.card title="ここだけcssを変更したい" class="bg-red-300"/>
+
+
 </x-tests.app>
