8b4a9e4e4aa6318cc9545b0a506540d299df813b
harada <pokosuke1004@gmail.com>
2025-02-25 18:51:12 +0900

    名前付きスロット

--- resources/views/tests/test1.blade.php
+++ resources/views/tests/test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+  <x-slot name="header">こちらはもう一つのslot,headerの方です</x-slot>
   コンポーネントテスト１
 </x-tests.app>
--- resources/views/tests/test2.blade.php
+++ resources/views/tests/test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+  <x-slot name="header">こちらはもう一つのslot,headerの方です.中身は変えています</x-slot>
   コンポーネントテスト２
 </x-tests.app>
