7a4819a27dc73931734e2633321e9aded02f2d83
harada <pokosuke1004@gmail.com>
2025-03-07 19:16:38 +0900

    create.blade作成

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -22,23 +22,20 @@ class OwnersController extends Controller
     
      public function index()
     {   
-        $date_now=Carbon::now();
-        $date_parse=Carbon::parse(now());
-        echo $date_now.'<br>';
-        echo $date_parse.'<br>';
-        echo $date_now->year.'<br>';
+        // $date_now=Carbon::now();
+        // $date_parse=Carbon::parse(now());
+        // echo $date_now.'<br>';
+        // echo $date_parse.'<br>';
+        // echo $date_now->year.'<br>';
 
-
-        
-        
-        $e_all=Owner::all();
-        $q_get=DB::table('owners')->select('name','created_at')->get();
+        $owners=Owner::select('name','email','created_at')->get();
+        // $q_get=DB::table('owners')->select('name','created_at')->get();
         // $q_first=DB::table('owners')->select('name')->first();
         // $c_test=collect([
         //     'name'=>'テスト'
         // ]);
         // dd($e_all,$q_get,$q_first,$c_test);
-        return view('admin.owners.index',compact('e_all','q_get'));
+        return view('admin.owners.index',compact('owners'));
     }
 
     /**
@@ -48,7 +45,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view ('admin.owners.create');
     }
 
     /**
