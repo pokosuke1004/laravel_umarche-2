7a2afee9e7cfdee43f60ce7d222287f052dda0be
harada <pokosuke1004@gmail.com>
2025-03-08 01:10:18 +0900

    storeとvalidationとflash

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner;//エロクエント
 use Illuminate\Support\Facades\DB;//クエリビルダ
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
 
 class OwnersController extends Controller
 {
@@ -56,7 +58,19 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        $request->validate([
+          'name'=>'required | string | max:255',
+          'email'=>'required |string | email | max:255 | unique:owners',
+          'password'=>'required | string | confirmed |min:8',
+
+        ]);
+        Owner::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ]);
+        return redirect()->route('admin.owners.index')->with('message','オーナーを追加登録しました');
     }
 
     /**
