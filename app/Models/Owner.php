e0af0770eceb0855e55cef3a8201a9a6fd1a6405
harada <pokosuke1004@gmail.com>
2025-03-11 11:04:24 +0900

    shop追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\softDeletes;
+use App\Models\Shop;
 
 
 class Owner extends Authenticatable
@@ -41,5 +42,9 @@ class Owner extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];   use HasFactory;
+    ];   
+
+    public function shop(){
+        return $this->hasOne(Shop::class);
+    }
 }
---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('shops')->insert([
+            [
+            'owner_id'=>1,
+            'name'=>'店名',
+            'information'=>'ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。',
+            'filename'=>'',
+            'is_selling'=>true,
+            ],
+            [
+            'owner_id'=>2,
+            'name'=>'店名',
+            'information'=>'ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。',
+            'filename'=>'',
+            'is_selling'=>true,
+            ],
+            [
+            'owner_id'=>3,
+            'name'=>'店名',
+            'information'=>'ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。ここに店舗情報が入ります。',
+            'filename'=>'',
+            'is_selling'=>true,
+            ],
+        ]);
+    }
+}
