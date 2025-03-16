6d5199b8637989b736e7e12b24881efb136d1798
harada <pokosuke1004@gmail.com>
2025-03-16 12:41:44 +0900

    edit_update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
-use App\Http\Requests\UploadImmageRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
 
 class ShopController extends Controller
@@ -44,14 +46,29 @@ class ShopController extends Controller
 
     }
 
-    public function update(UploadImmageRequest $request){
+    public function update(UploadImageRequest $request, $id){
+       
+        $request->validate([
+            'name'=>'required|string|max:50',
+            'information'=>'required|string|max:1000',
+            'is_selling'=>'required',
+        ]);
+       
+
        $imageFile= $request->image;
        if(!is_null($imageFile)&&$imageFile->isValid()){
         $fileNameToStore=ImageService::upload($imageFile,'shops');
-        // Storage::putFile('public/shops',$imageFile);
         
-        // dd($imageFile,$resizedImage);
        }
-       return redirect()->route('owner.shops.index');
+
+       $shop=Shop::findOrFail($id);
+       $shop->name = $request->name;
+       $shop->information = $request->information;
+       $shop->is_selling = $request->is_selling;
+       if(!is_null($imageFile) && $imageFile->isValid()){
+        $shop->filename=$fileNameToStore;
+       }
+       $shop->save();
+       return redirect()->route('owner.shops.index')->with(['message'=>'店舗情報を更新しました','status'=>'info']);
     }
 }
--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -13,6 +13,8 @@ class ImageService{
         
     $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
     Storage::put('public/'.$folderName.'/' . $fileNameToStore, $resizedImage );
+     
+    return $fileNameToStore;
 
 
   }
