a4fa9645895dfbf06fc66be9a9bce878a2291603
harada <pokosuke1004@gmail.com>
2025-03-20 23:11:40 +0900

    store

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
+use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ImageController extends Controller
 {
@@ -31,7 +33,10 @@ class ImageController extends Controller
 
     public function index()
     {
-        $images=Image::where('owner_id',Auth::id())->orderBy('updated_at','desc')->paginate(20);
+        $images=Image::where('owner_id',Auth::id())
+        ->orderBy('updated_at','desc')
+        ->paginate(20);
+        
         return view('owner.images.index',compact('images'));
     }
 
@@ -42,7 +47,7 @@ class ImageController extends Controller
      */
     public function create()
     {
-        //
+        return view('owner.images.create');
     }
 
     /**
@@ -51,9 +56,20 @@ class ImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UploadImageRequest $request)
     {
-        //
+       $imageFiles = $request->file('files');
+       if(!is_null($imageFiles)){
+        foreach($imageFiles as $imageFile){
+            $fileNameToStore=ImageService::upload($imageFile,'products');
+            Image::create([
+                'owner_id'=>Auth::id(),
+                'filename'=>$fileNameToStore,
+            ]);
+        }
+       }return redirect()
+       ->route('owner.images.index')
+       ->with(['message'=>'画像登録を実施しました','status'=>'info']);
     }
 
     /**
--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -7,11 +7,17 @@ use InterventionImage;
 
 class ImageService{
   public static function upload($imageFile,$folderName){
+if(is_array($imageFile)){
+  $file = $imageFile['image'];
+}else{
+  $file=$imageFile;
+}
+
     $fileName = uniqid(rand().'_'); //重複しないファイル名
-    $extension = $imageFile->extension();//取得した画像に拡張子をつける。
+    $extension = $file->extension();//取得した画像に拡張子をつける。
     $fileNameToStore = $fileName. '.' . $extension;
         
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+    $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
     Storage::put('public/'.$folderName.'/' . $fileNameToStore, $resizedImage );
      
     return $fileNameToStore;
