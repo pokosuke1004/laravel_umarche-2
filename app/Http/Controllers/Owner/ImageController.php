bdf519db70e9bc2212b0f6addf857e4ae99a71e5
harada <pokosuke1004@gmail.com>
2025-03-27 18:23:58 +0900

    delete

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
+use Illuminate\Support\Facades\Storage;
+
 
 class ImageController extends Controller
 {
@@ -122,6 +124,18 @@ class ImageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $image=Image::findOrFail($id);
+
+        $filePath='public/products/'.$image->filename;
+        
+        if(Storage::exists($filePath)){
+        Storage::delete($filePath);  
+        }      
+        
+        Image::findOrFail($id)->delete();
+
+        return redirect()->route('owner.images.index')->with(['message'=>'画像を廃棄しました','status'=>'alert']);
+        
+        
     }
 }
