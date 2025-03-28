1a46ade6b8b8cd83740aa22bca880eb1941f11c1
harada <pokosuke1004@gmail.com>
2025-03-28 19:19:28 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
+            CategorySeeder::class,
+
         ]
 
         );
