9d69099a20b696664354d6e9fd26889571e3709e
harada <pokosuke1004@gmail.com>
2025-03-29 15:21:44 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 class Product extends Model
 {
@@ -29,6 +30,9 @@ class Product extends Model
     }
     public function imageForth(){
         return $this->belongsTo(Image::class,'image4','id');
+    }
+    public function stock(){
+        return $this->hasMany(Stock::class);
     }
     
 }
