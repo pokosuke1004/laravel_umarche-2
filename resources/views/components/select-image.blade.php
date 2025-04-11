b31f45c0d4b94a99d08ec61e1b9c2521e0e83839
harada <pokosuke1004@gmail.com>
2025-04-11 15:12:17 +0900

    product_create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -6,7 +6,7 @@ if($name === "image4"){ $modal = 'modal-4';}
 @endphp
 
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-  <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+  <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
     <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
       <header class="modal__header">
         <h2 class="text-xl text-gray-700" id="{{ $modal }}-title">
