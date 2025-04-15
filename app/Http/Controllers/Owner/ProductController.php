660281f498a0b497d00accd95a45627e429c71dd
harada <pokosuke1004@gmail.com>
2025-04-15 10:42:38 +0900

    destroy

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -219,6 +219,13 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product=Product::findOrFail($id)->delete();
+
+        return redirect()->route('owner.products.index')
+        ->with([
+            'message'=>'商品を削除しました',
+            'status'=>'alert',
+        ]);
+
     }
 }
--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -113,10 +113,17 @@
                           <button type="submit" class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                         </div>
                   </div>
-                    
-                   
+               </form>
+               <form id="delete_{{$product->id}}" method="post" action="{{route('owner.products.destroy',['product'=>$product->id])}}">
+                @csrf
+                @method('delete')
+                <div class="p-2 w-full flex justify-around mt-32">
+                  <a href="#" data-id="{{$product->id}}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">削除する</a>
+                </div>
 
-                  </form>
+
+
+              </form>
               </div>
           </div>
       </div>
@@ -137,5 +144,11 @@
  MicroModal.close(modal); //モーダルを閉じる
 })
 })
+function deletePost(e){
+    'use strict';
+    if(confirm('本当に削除してもいいですか')){
+      document.getElementById('delete_'+ e.dataset.id).submit();
+    }
+  }
 </script>
 </x-app-layout>
