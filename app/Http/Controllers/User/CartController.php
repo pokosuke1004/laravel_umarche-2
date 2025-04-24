82ca4e083b7fb71742b8fd5e4baa97f8e5ac2e94
harada <pokosuke1004@gmail.com>
2025-04-24 22:42:56 +0900

    ストライプキャンセル処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -115,6 +115,19 @@ class CartController extends Controller
         
         return redirect()->route('user.items.index');
     }
+
+    public function cancel(){
+        $user=User::findOrFail(Auth::id());
+        
+        foreach($user->products as $product){
+            Stock::create([
+                'product_id'=>$product->id,
+                'type'=>\Constant::PRODUCT_LIST['add'],
+                'quantity'=>$product->pivot->quantity,
+            ]);
+        }
+        return redirect()->route('user.cart.index');
+    }
 }
 
 
