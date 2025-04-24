eb04987d525e3077ee5c3ba07a8a9e2027ebbc57
harada <pokosuke1004@gmail.com>
2025-04-24 22:16:13 +0900

    決済後のカート内商品削除

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -100,7 +100,7 @@ class CartController extends Controller
             'payment_method_types' => ['card'], 
             'line_items'=>[$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
+            'success_url' => route('user.cart.success'),
             'cancel_url' => route('user.cart.cancel'),
             ]);
 
@@ -110,5 +110,11 @@ class CartController extends Controller
         // return view('user.checkout',compact('session','publicKey'));
         return redirect($session->url,303);
     }
+    public function success(){
+        Cart::where('user_id',Auth::id())->delete();
+        
+        return redirect()->route('user.items.index');
+    }
 }
+
 
